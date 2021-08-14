ae01c67d476edfab7129b69e7eec9779854d2768
kmcelikcan <65337272+kmcelikcan@users.noreply.github.com>
2021-08-14 21:49:34 +0300

    Update odev_1.sql
    1. Ödev sonuçları

--- odev_1.sql
+++ odev_1.sql
@@ -4,7 +4,7 @@
 WHERE length > 60 AND length < 75;
 
 3-SELECT * FROM film
-WHERE rental_rate = 0.99 AND (replacement_cost = 12.99 OR replacement_cost = 28.99) ;
+WHERE rental_rate = 0.99 AND (replacement_cost = 12.99 OR replacement_cost = 28.99 ) ;
 
 4-SELECT last_name FROM customer
 WHERE first_name = 'Mary'
